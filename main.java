6c96e299c83c107f612e44a8120bb13c29e52a77
Eluyen70 <eluyen70@gmail.com>
2025-08-17 13:51:50 -0400

    Update main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args){
+        System.out.print("Hello, World!");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Kenton is helping\n" + i);
+        }
+    }
+}
