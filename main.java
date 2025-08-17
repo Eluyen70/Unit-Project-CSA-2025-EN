cbcec788e54ee3519eb5e2c4ce2bb859474de5f7
Eluyen70 <eluyen70@gmail.com>
2025-08-17 15:41:23 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,7 @@
+/*Elvis Nguyen
+  08/15/2025
+  APCSA Unit 0 Project
+    This program prints out my initials in a bordered box. B is the middle initial because I don't have a middle name.*/
 class Main {
     public static void main(String[] args){
         System.out.print("------------------------------------------------------------------------------------------------------\n| abcdefghijklmnopqrstuvwxyz          abcdefghijklmnopqrstuv              abcdefghij     pqrstuvwxyz |\n| zyxwvutsrqponmlkjihgfedcba          bcdefghijklmnopqrstuvwxyz           abcdefghijk    pqrstuvwxyz |\n| abcdefghijk                         cdefghijklm      tuvwxyzab          abcdefghijkl   pqrstuvwxyz |\n| kjihgfedcba                         defghijklmn      uvwxyzabc          abcdefghijklm  pqrstuvwxyz |\n| abcdefghijklmnopqrstuvwxyz          efghijklmnopqrstuvwxyza             abcdefghijklmn pqrstuvwxyz |\n| zyxwvutsrqponmlkjihgfedcba          fghijklmnopqrstuvwxyzabc            abcdefghijklmnopqrstuvwxyz |\n| abcdefghijk                         ghijklmnopq      xyzabcde           abcdefghij lmnopqrstuvwxyz |\n| kjihgfedcba                         hijklmnopqr      yzabcdefg          abcdefghij  mnopqrstuvwxyz |\n| abcdefghijklmnopqrstuvwxyz          ijklmnopqrstuvwxyzabcdefgh          abcdefghij   nopqrstuvwxyz |\n| zyxwvutsrqponmlkjihgfedcba          jklmnopqrstuvwxyzabcdefgh           abcdefghij    opqrstuvwxyz |\n------------------------------------------------------------------------------------------------------\n");
